6cbe56f85b40b46a99d82a060f602e24aa2094c6
SaifeeBurhaniCreations <saifeeburhanicreations@gmail.com>
2026-01-22 15:49:27 +0530

    chore(mobile): remove deprecated components and services

--- oddiville-mobile/src/services/productItem.service.ts
+++
@@ -1,6 +0,0 @@
-import api from "../lib/axios";
-import { CreatePackedItemDTO } from "../utils/mapStorageFormToPackedDTO";
-
-export const createPackedItem = (data: CreatePackedItemDTO) => api.post(`/packed-item`, data);
-export const getPackedItems = () => api.get(`/packed-item`);
-export const getPackedItemsToday = () => api.get(`/packed-item/packing-summary/today`);
